1b6ad22020b703e0119533163a4534169073f72b
Sebastián Berardi <sebastianberardi2@gmail.com>
2025-03-25 19:37:58 -0300

    xd

---
+++ Hola mundo/Holamundo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void main(void) {
+    printf("hola mundo");
+}
---
+++ sizeof/sizeof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+    printf("Tamaño de int: %zu bytes\n", sizeof(int));
+    printf("Tamaño de short: %zu bytes\n", sizeof(short));
+    printf("Tamaño de char: %zu bytes\n", sizeof(char));
+    printf("Tamaño de long: %zu bytes\n", sizeof(long));
+    printf("Tamaño de long long: %zu bytes\n", sizeof(long long));
+    printf("Tamaño de float: %zu bytes\n", sizeof(float));
+    printf("Tamaño de double: %zu bytes\n", sizeof(double));   
+}
