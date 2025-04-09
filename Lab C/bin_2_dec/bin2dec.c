3f0b11286c8b3f5f63b312ccd926ffb841e45216
Sebastián Berardi <sebastianberardi2@gmail.com>
2025-04-08 21:08:03 -0300

    otros cambios

--- Lab C/bin_2_dec/bin2dec.c
+++ Lab C/bin_2_dec/bin2dec.c
@@ -5,22 +5,20 @@
 
 int main() //int32_t binary, bool sign
 {
-    printf("\n\n-----------------------------------------------------\n\n");
+    printf("-----------------------------------------------------\n\n");
     
     
     char bin[] = "111100101101101110001";
     //strcpy(bin,binary)
     int valor_final = 0;
     int valor_parcial = 0;
-    int signo = 1; //bool sign
+    int signo = 1; //bool sign //editar, true or false
 
     int len = strlen(bin);    
 
     for ( int bit = 0; bit+1 <= len; bit++ ){
 
         if (bin[bit] == '1'){
-            printf("%d; " ,len-1-bit);
-            printf("%c\n",bin[bit]);
             valor_parcial = 1 * pow(2, (len-1-bit));
             valor_final = valor_final + valor_parcial;
         }
@@ -29,13 +27,13 @@ int main() //int32_t binary, bool sign
         }
     }
     if (signo == '1'){
-        printf ("\nTu número es -%d?",valor_final);
+        printf ("Tu número es -%d?",valor_final);
     }
     else{
-        printf ("\nTu número es %d?",valor_final);
+        printf ("Tu número es %d?",valor_final);
     }
     
     
-    printf("\n\n-----------------------------------------------------\n\n");
+    printf("\n\n-----------------------------------------------------");
     
 }
