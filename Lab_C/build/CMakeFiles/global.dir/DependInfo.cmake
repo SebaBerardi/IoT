466072ceeb0edff657014d0470f4941b2db2ce38
Sebastián Berardi <sebastianberardi2@gmail.com>
2025-04-20 03:15:44 -0300

    Movi algunos archivos de lugar
    Deje los archivos del lab C en donde van

---
+++ Lab_C/build/CMakeFiles/global.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/bin2dec.c" "CMakeFiles/global.dir/src/bin2dec.c.obj" "gcc" "CMakeFiles/global.dir/src/bin2dec.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/complex.c" "CMakeFiles/global.dir/src/complex.c.obj" "gcc" "CMakeFiles/global.dir/src/complex.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/conNvocs.c" "CMakeFiles/global.dir/src/conNvocs.c.obj" "gcc" "CMakeFiles/global.dir/src/conNvocs.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/days_left.c" "CMakeFiles/global.dir/src/days_left.c.obj" "gcc" "CMakeFiles/global.dir/src/days_left.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/eq_solver.c" "CMakeFiles/global.dir/src/eq_solver.c.obj" "gcc" "CMakeFiles/global.dir/src/eq_solver.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/find_in_sting.c" "CMakeFiles/global.dir/src/find_in_sting.c.obj" "gcc" "CMakeFiles/global.dir/src/find_in_sting.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/init_lab.c" "CMakeFiles/global.dir/src/init_lab.c.obj" "gcc" "CMakeFiles/global.dir/src/init_lab.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/inverse_array.c" "CMakeFiles/global.dir/src/inverse_array.c.obj" "gcc" "CMakeFiles/global.dir/src/inverse_array.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/matrixsub.c" "CMakeFiles/global.dir/src/matrixsub.c.obj" "gcc" "CMakeFiles/global.dir/src/matrixsub.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/max-min.c" "CMakeFiles/global.dir/src/max-min.c.obj" "gcc" "CMakeFiles/global.dir/src/max-min.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/reverse_string.c" "CMakeFiles/global.dir/src/reverse_string.c.obj" "gcc" "CMakeFiles/global.dir/src/reverse_string.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/string_copy.c" "CMakeFiles/global.dir/src/string_copy.c.obj" "gcc" "CMakeFiles/global.dir/src/string_copy.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/string_length.c" "CMakeFiles/global.dir/src/string_length.c.obj" "gcc" "CMakeFiles/global.dir/src/string_length.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/string_to.c" "CMakeFiles/global.dir/src/string_to.c.obj" "gcc" "CMakeFiles/global.dir/src/string_to.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/string_words.c" "CMakeFiles/global.dir/src/string_words.c.obj" "gcc" "CMakeFiles/global.dir/src/string_words.c.obj.d"
+  "C:/Users/Seba/Documents/GitHub/IoT/Lab_C/src/swap.c" "CMakeFiles/global.dir/src/swap.c.obj" "gcc" "CMakeFiles/global.dir/src/swap.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
